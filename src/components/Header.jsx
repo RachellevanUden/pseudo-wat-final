231ad2968696ddf58445b527e7652e299917d009
RachellevanUden <rachelle.van.uden@student.ehb.be>
2025-04-23 15:21:19 +0200

    Added Header component with styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import headerImage from '../assets/header-image.webp'
+import '../styles/Header.css'
+
 function Header() {
-    return <h1>Welkom bij 'Pseudo-wat?' Jouw gids voor Pseudomyxoma Peritonei</h1>
-}
+    return (
+      <header className="hero-header">
+        <img src={headerImage} alt="Decoratieve orange flow" className="header-bg" />
+        <div className="header-text">
+          <h1>Welkom bij 'Pseudo-wat?' <br/>Jouw gids voor Pseudomyxoma Peritonei</h1>
+        </div>
+      </header>
+    )
+  }
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Header from '../components/Header'
+import TextBlock from '../components/TextBlock'
+
 function Home() {
     return (
-        <div> 
+        <> 
+        <Header />
+            <main className='container'>
             <h2>Waarom ik dit platform maakte</h2>
-            <p>Coming soon...</p>
-        </div>
+            <p>Coming soon...</p>                   
+            </main>
+        </>
     );
 }
 
