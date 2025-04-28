35c981e38564b0048046b8be3285930213bcaa5e
RachellevanUden <rachelle.van.uden@student.ehb.be>
2025-04-28 15:52:41 +0200

    feat: make pages responsive + add hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,43 @@
-import { NavLink } from 'react-router-dom'
-import logo from '/pmp-logo.svg'
-import '../styles/Navbar.css'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '/pmp-logo.svg';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdClose } from 'react-icons/io'; // sluit-icoon
+import '../styles/Navbar.css';
 
-function Navbar () {
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav>
       <div className="nav-container">
-        <div className='logo'>
-          <img src={logo} alt='Pseudo-wat logo' />
+        <NavLink to="/" className="logo" onClick={closeMenu}>
+          <img src={logo} alt="Pseudo-wat logo" />
+        </NavLink>
+
+        {/* Hamburger icon */}
+        <div className="hamburger" onClick={toggleMenu}>
+          {menuOpen ? <IoMdClose size={30} /> : <GiHamburgerMenu size={30} />}
         </div>
-        
-        <ul>
-          <li><NavLink to="/" end>Home</NavLink></li>
-          <li><NavLink to="/WatIsPmp">Wat is PMP?</NavLink></li>
-          <li><NavLink to="/Behandelingsopties">Behandelingsopties</NavLink></li>
-          <li><NavLink to="/OverMij">Over mij</NavLink></li>
+
+        {/* Menu links */}
+        <ul className={menuOpen ? 'nav-links open' : 'nav-links'}>
+          <li><NavLink to="/" end onClick={closeMenu}>Home</NavLink></li>
+          <li><NavLink to="/WatIsPmp" onClick={closeMenu}>Wat is PMP?</NavLink></li>
+          <li><NavLink to="/Behandelingsopties" onClick={closeMenu}>Behandelingsopties</NavLink></li>
+          <li><NavLink to="/OverMij" onClick={closeMenu}>Over mij</NavLink></li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
